8e120421b226a14cfeb94ba17636f8daec8a3c9e
odin <liorodin@gmail.com>
2021-12-03 23:39:13 +0200

    new update

--- client.py
+++ client.py
@@ -3,30 +3,47 @@ import time
 import sys
 import os
 import string
+import random
 
 #finals
 BUFFER_SIZE = 2300
 END_MARK = '.'
 DEFAULT_ID = '0'
+LOCAL_ID_LEN = 36
 ID_LEN = 128
 FOLDER_MARK = '@'
 FILE_MARK = 'f'
+ACCEPT = 'ok'
 
 try:
     DEST_IP = sys.argv[1]
     DEST_PORT = int(sys.argv[2])
     #FILE_PATH = sys.argv[3]
-    DIRECTORY_PATH = '/home/odin/Desktop/test file'
-    #DIRECTORY_PATH = '/home/odin/Desktop/new'
+    #DIRECTORY_PATH = '/home/odin/Desktop/test file'
+    DIRECTORY_PATH = '/home/odin/Desktop/new1'
     TIME_INTERVAL = sys.argv[4]
+    
 except:
-    sys.exit()
+    print()
 
 try:
     ID_NUM = sys.argv[5]
 
 except:
     ID_NUM = DEFAULT_ID
+DEST_PORT = 12550
+DIRECTORY_PATH = '/home/odin/Desktop/new1'
+
+#
+#class Handler(watchdog.events.PatternMatchingEventHandler):
+  #  def __init__(self):
+ #       watchdog.events.PatternMatchingEventHandler(self, Pattern=['*.*'], ignore_patterns = None,
+  #       ignore_directories = False, cade_sensitive = True)
+ #   def on_created(self, event):
+ #       print('File was created at {event.src_path}')
+  #  def on_deleted(self, event):
+  #      print('File was deleted at {event.src_path}')
+
 
 def get_folder_files(folder_files , s, folder_path):
     for file_name in folder_files:
@@ -53,7 +70,7 @@ def get_folder_files(folder_files , s, folder_path):
                 #waits for server acceptance
                 s.recv(BUFFER_SIZE)
                 data = x.read(BUFFER_SIZE)              
-            s.send(END_MARK.encode())
+            s.send(str(END_MARK).encode())
             #waits for server acceptance
             s.recv(BUFFER_SIZE)
     s.send(END_MARK.encode())
@@ -64,28 +81,102 @@ def new_id_protocol(s):
     #file transer sequence
     get_folder_files(folder_files, s, DIRECTORY_PATH)
 
-def existing_id_protocol(s):
-    print(
+def insert_new_folder(folder_path, client_socket):
+    #file transfer sequence
+    file_data = client_socket.recv(BUFFER_SIZE).decode()
+    while(file_data != END_MARK):
+        file_name = file_data[1:]
+        data_type = file_data[0]
         
-    )
+        if data_type == FOLDER_MARK:
+            os.makedirs(folder_path + file_name)
+            new_folder_path = folder_path + file_name + '/'   
+            #sends approval       
+            client_socket.send(ACCEPT.encode())
+            #insert file of the new folder
+            insert_new_folder(new_folder_path, client_socket)
+            #sends approval
+            client_socket.send(ACCEPT.encode())
+            
+        else:
+            client_socket.send(ACCEPT.encode())
+            #file creation sequence
+            new_file = folder_path + file_name
+            f = open(new_file,'wb')
+            #gets file bytes from client
+            data = client_socket.recv(BUFFER_SIZE)
+            try:
+                end_check = data.decode()
+            except:
+                end_check = 0
+            while(end_check != END_MARK):
+                f.write(data)
+                client_socket.send(ACCEPT.encode())
+                data = client_socket.recv(BUFFER_SIZE)
+                try:
+                    end_check = data.decode()
+                except:
+                    end_check = 0
+            f.close()
+            #sends approval
+            client_socket.send(ACCEPT.encode())
+            #gets new file name
+        file_data = client_socket.recv(BUFFER_SIZE).decode()
 
-def run_client(ID_NUM):
+
+def existing_id_protocol(s):
+    folder_path = DIRECTORY_PATH + '/'
+    insert_new_folder(folder_path, s)
+    
+def local_id_generator(size = LOCAL_ID_LEN, chars=string.ascii_uppercase + string.digits):
+    new_id = ''.join(random.choice(chars) for _ in range(size))
+    return new_id
+
+
+#main
+
+local_id = local_id_generator()   
+#event_handler = Handler()
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+#s.connect((DEST_IP, DEST_PORT))
+s.connect(('10.0.2.5', DEST_PORT))
+#sends identification
+id = ID_NUM + ',' + local_id
+s.send(str(id).encode())
+#waits for server's acceptance
+s.recv(BUFFER_SIZE)
+
+#new id protocol
+if(ID_NUM == DEFAULT_ID):
+
+    #gets an id from server
+    ID_NUM = s.recv(ID_LEN).decode()
+    id = ID_NUM + ',' + local_id
+    new_id_protocol(s)        
+    
+else:
+    existing_id_protocol(s)
+   
+s.close()
+while(True):
+
+    #time.sleep(TIME_INTERVAL)
+    time.sleep(3)
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
     #s.connect((DEST_IP, DEST_PORT))
-    s.connect(('10.0.2.5', 12353))
-    #sends identification
-    s.send(ID_NUM.encode())
+    s.connect(('10.0.2.5', DEST_PORT))
+    s.send(str(id).encode())
+    #waits for server's acceptance
+    s.recv(BUFFER_SIZE)
 
-    #new id protocol
-    if(ID_NUM == DEFAULT_ID):
-        #gets an id from server
-        ID_NUM = s.recv(ID_LEN).decode()
-        new_id_protocol(s)
+    #imitates a regular state
+    s.send('0'.encode())
+
+    #imitates a change state
+    #s.send('1'.encode())
+
+    new_data = s.recv(6).decode()
+    if new_data == 'update':
+        print('update')
     s.close()
-
-def main():
-    run_client(ID_NUM)
-
-if __name__ == '__main__':
-    main()
--- server.py
+++ server.py
@@ -4,6 +4,7 @@ import random
 import string
 import os
 import sys
+import watchdog
 
 #finals
 BUFFER_SIZE = 2300
@@ -19,8 +20,13 @@ try:
 except:
     DEST_IP = 0
 
+DEST_IP = 12550
+
 #list of client ids
 ID_LIST = []
+CONNECTED_USERS = {}
+USER_DICT = {}
+
 SERVER_FOLDER = os.path.dirname(os.path.abspath(__file__)) + '/'
 
 def id_generator(size = ID_LEN, chars=string.ascii_uppercase + string.digits):
@@ -36,6 +42,7 @@ def insert_new_folder(folder_path, client_socket):
     while(file_data != END_MARK):
         file_name = file_data[1:]
         data_type = file_data[0]
+        
         if data_type == FOLDER_MARK:
             os.makedirs(folder_path + file_name)
             new_folder_path = folder_path + file_name + '/'   
@@ -71,33 +78,101 @@ def insert_new_folder(folder_path, client_socket):
             #gets new file name
         file_data = client_socket.recv(BUFFER_SIZE).decode()
 
-def new_id_protocol(client_socket):
-    #generates a new id number for client
-    id_num = id_generator()
-    #sends to client his new id number
-    client_socket.send(id_num.encode())
-    print(id_num)
+def new_id_protocol(client_socket,client_id, client_pc):
+
+    #enter a new connection of the client
+    USER_DICT[client_id] = [client_pc]
+    CONNECTED_USERS[client_pc] = []
+
     #makes a new folder in the server folder for the files of the client
-    os.makedirs(SERVER_FOLDER + id_num)
-    folder_path = SERVER_FOLDER + id_num + '/'
-    insert_new_folder(folder_path, client_socket)  
+    os.makedirs(SERVER_FOLDER + client_id)
+    folder_path = SERVER_FOLDER + client_id + '/'
+    insert_new_folder(folder_path, client_socket)
 
-def run_server():
-    server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    server.bind(('', 12353))
-    server.listen(5)
+def get_folder_files(folder_files , s, folder_path):
+    for file_name in folder_files:
+        file_path = folder_path + '/' + file_name
+        if os.path.isdir(file_path):
+            s.send(str(FOLDER_MARK + file_name).encode())           
+            #waits for client's acceptance
+            s.recv(BUFFER_SIZE)
+            #transfer a new folder inside current folder
+            new_folder = os.listdir(file_path)
+            get_folder_files(new_folder , s ,file_path)
+            #waits for client's acceptance
+            s.recv(BUFFER_SIZE)
+    
+        else:
+            s.send(str(FILE_MARK + file_name).encode())
+            #waits for client's acceptance
+            s.recv(BUFFER_SIZE)
+            x = open(file_path, 'rb')
+            #reads and sends bytes
+            data = x.read(BUFFER_SIZE)
+            while(data):
+                s.send(data)
+                #waits for client's acceptance
+                s.recv(BUFFER_SIZE)
+                data = x.read(BUFFER_SIZE)              
+            s.send(END_MARK.encode())
+            #waits for client's acceptance
+            s.recv(BUFFER_SIZE)
+    s.send(END_MARK.encode())
 
-    while True:
-        client_socket, client_address = server.accept()
-        data = client_socket.recv(BUFFER_SIZE)
-        #new id protocol
-        if(data.decode() == DEFAULT_ID):
-            new_id_protocol(client_socket)
-            
+def existing_id_protocol(client_socket, client_id):   
+    client_folder_path = SERVER_FOLDER + client_id + '/'
+    folder_files = os.listdir(client_folder_path)
+    get_folder_files(folder_files, client_socket, client_folder_path)
+        
+#main
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server.bind(('', DEST_IP))
+server.listen(5)
+
+while True:
+    client_socket, client_address = server.accept()     
+    print(client_address[1],' connected')
+    data = client_socket.recv(BUFFER_SIZE).decode()
+    client_info = data.split(',')
+    client_id = client_info[0]
+    client_pc = client_info[1]  
+    client_socket.send(ACCEPT.encode())
+
+    #new id protocol
+    if(client_id == DEFAULT_ID):
+        #generates a new id number for client
+        client_id = id_generator()
+        print(client_id)
+        #sends to client his new sid number
+        client_socket.send(client_id.encode())
+        #print(client_address[1],' connected')
+        new_id_protocol(client_socket,client_id, client_pc)
         client_socket.close()
+        continue
+    else:
+        print(client_pc)
+        if(client_pc not in USER_DICT[client_id]):
+            print('new pc')
+            #add new client connection
+            USER_DICT[client_id].append(client_pc)
+            CONNECTED_USERS[client_pc] = []
+            existing_id_protocol(client_socket, client_id)
+            client_socket.close()
+            continue
 
-def main():
-    run_server()
+        new_data = client_socket.recv(BUFFER_SIZE).decode()
+        if new_data != '0':
+            for pc in USER_DICT[client_id]:
+                if pc != client_pc:
+                    CONNECTED_USERS[pc].append(new_data)
 
-if __name__ == '__main__':
-    main()
+        #pc operation stack isn't empty
+        if len(CONNECTED_USERS[client_pc]) != 0:
+            #while len(CONNECTED_USERS[client_pc]) != 0:
+            client_socket.send('update'.encode())
+            CONNECTED_USERS[client_pc].pop(0)
+        else:
+            client_socket.send('reg'.encode())
+
+    client_socket.close()
+    print(client_address,' disconnected')
